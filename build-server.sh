7e7f3bcd49abb13e94f9a3e33c1edb42f2dea67d
EaterMiner <58890567+Eaterminer@users.noreply.github.com>
2025-01-19 19:10:11 +1300

    build: Fixes ARM building

--- build-server.sh
+++ build-server.sh
@@ -9,12 +9,11 @@ if [ ! -d "$MKOSI_DIR" ]; then
 	git clone https://github.com/systemd/mkosi "$MKOSI_DIR"
 fi
 
-# Clear old builds
-"$MKOSI_DIR/bin/mkosi" clean -f -f
-
 # Build
 time "$MKOSI_DIR/bin/mkosi" \
 	--compress-output=zstd \
 	--source-date-epoch=0 \
 	--seed=5678 \
+	--force \
+	--profile=arm
 # Most of these args are just reproducability.
